09a97fccb4d1f45178b1106b5053b9dd7a728749
Luke Dunscombe <ldunscombe@leantechniques.com>
2020-03-11 09:08:11 -0500

    Updating bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,12 @@
 
-# TODO : Delete symlinks to deleted files
 link () {
 	echo "$PROMPT This utility will symlink the files in this repo to the home directory"
 	echo "$PROMPT Proceed? (y/n)"
 	read resp
 	# TODO - regex here?
 	if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
-		for file in $( ls -A | grep -vE '\bootstrap.sh|\.git$|\.gitignore|.*.md' ) ; do
-			ln -sv "$PWD/$file" "$HOME"
+		for file in $( ls -A | grep -vE 'bootstrap.sh|\.git$|.*.md' ) ; do
+			ln -sfnv "$PWD/$file" "$HOME"
 		done
 		echo "$PROMPT Symlinking complete"
 	else
